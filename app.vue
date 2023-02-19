95d8822c061364060bb50475547c9d9e4567a208
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2023-02-19 16:27:56 -0300

    feat: ✨ create right sidebar component

--- app.vue
+++ app.vue
@@ -1,19 +1,31 @@
 <script setup lang="ts">
-const darkMode = ref(true);
+const darkMode = ref(false);
 </script>
 
 <template>
   <div :class="{ dark: darkMode }">
-    <div class="min-h-full bg-red-200">
-      <div
-        class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5"
-      >
-        <!-- left sidebar -->
-        <div class="hidden bg-red-500 md:block xs-col-span1 xl:col-span-2">
-          <div class="sticky top 0">sidebar</div>
+    <div class="bg-white dark:bg-dim-900">
+      <div class="min-h-full">
+        <div
+          class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5"
+        >
+          <!-- left sidebar -->
+          <div class="hidden md:block xs:col-span-1 xl:col-span-2">
+            <div class="sticky top 0">
+              <SidebarLeft />
+            </div>
+          </div>
+          <!-- main content -->
+          <main class="col-span-12 md:col-span-8 xl:col-span-6 bg-red-300">
+            main content
+          </main>
+          <!-- right sidebar -->
+          <div class="hidden md:block xl:col-span-4 md:col-span-3 bg-blue-300">
+            <div class="sticky top 0">
+              <SidebarRight />
+            </div>
+          </div>
         </div>
-        <!-- main content -->
-        <!-- right sidebar -->
       </div>
     </div>
   </div>
