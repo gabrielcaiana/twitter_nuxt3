91886e384cf54d28fcb98b2ee1a178e85742d797
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2023-02-19 15:37:41 -0300

    feat: ✨ create tab component

---
+++ components/Sidebar/Left/Tab.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const { defaultTransition } = useTailwindConfig();
+
+const props = defineProps<{
+  active: boolean;
+}>();
+
+const textClasses = computed<string>(() =>
+  props.active ? 'font-semibold' : 'font-normal'
+);
+</script>
+
+<template>
+  <nuxt-link
+    to="#"
+    :class="[
+      'flex items-center text-black p-3 w-min hover:bg-gray-200 rounded-full gap-4 dark:hover:bg-dim-200 dark:text-white',
+      defaultTransition,
+    ]"
+  >
+    <div class="text-dark">
+      <slot name="icon" />
+    </div>
+    <div :class="['text-xl hidden xl:block', textClasses]">
+      <slot name="name" />
+    </div>
+  </nuxt-link>
+</template>
